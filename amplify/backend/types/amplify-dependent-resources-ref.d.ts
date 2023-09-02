4a49de05e865d7e69f65b5900e7a5de340a205ad
Mordechai Dror <vorant94@gmail.com>
2023-09-02 10:32:43 +0300

    amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "serverlesscontainers": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
